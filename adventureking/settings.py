f4e4738120b2a2a14f0d7a48c9e915f409a53b08
Robert Murray <rob@wildmonkeybytes.com>
2016-08-22 16:56:22 -0400

    upated static path

--- adventureking/settings.py
+++ adventureking/settings.py
@@ -123,4 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
