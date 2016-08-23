0c31d18e5f64bd9dee45be97911691f3020fc339
Robert Murray <rob@wildmonkeybytes.com>
2016-08-23 09:49:09 -0400

    default to afield

--- adventureking/urls.py
+++ adventureking/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^.*$', include('afield.urls')),
     url(r'^afield/', include('afield.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- afield/urls.py
+++ afield/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'afield'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-
     url(r'^(?P<room_id>[0-9]+)/$', views.index, name='index'),
     url(r'^(?P<room_id>[0-9]+)/(?P<previous_room_action_id>[0-9]+)/$', views.index, name='index'),
 ]
